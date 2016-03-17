7c9fe73033c25c7b89ab821ee12f5590fd6ace11
vtegan <vtegan@users.noreply.github.com>
2016-03-16 21:20:05 -0400

    Add src dir with a file.

---
+++ src/org/vegan/test/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vegan.test;
+
+/**
+ * @author egan
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to fundamental Design Patterns.");
+
+	}
+
+}
