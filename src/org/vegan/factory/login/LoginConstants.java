38775cfc15d3bb8aa94e1ef24b784e5610805062
vtegan <vtegan@rochester.rr.com>
2016-03-22 10:57:40 -0400

    Added constatnce file.

---
+++ src/org/vegan/factory/login/LoginConstants.java
@@ -0,0 +1,26 @@
+package org.vegan.factory.login;
+
+public class LoginConstants {
+	
+	
+	public static final String DAO_FACTORY_PROPERTY = "dao";
+	public static final String DAO_FACTORY_UNIT_TEST_PROPERTY = "dao.test";
+	public static final Class<UserBeanHolder> BEAN_CLASS = UserBeanHolder.class;
+	public static final String BEAN_NAME = "userBeanHolder";
+	public static final String SPRING_CONFIG_FILE = "/config/Beans.xml";
+	
+	
+	//Constants below are for testing purposes.
+	public static final String BAD_PASSWORD = "jills";
+	public static final String BAD_USER = "began";
+	
+	public static final String PASSWORD_TWO = "giants";
+	public static final String USER_TWO = "jjones";
+	
+	public static final String PASSWORD_ONE = "bills";
+	public static final String USER_ONE = "tegan";
+	
+	public static final String PASSWORD_THREE = "patriots";
+	public static final String USER_THREE = "pdoran";
+
+}
