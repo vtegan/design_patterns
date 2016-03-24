7a90744f80c6f54cc6ec5238f21786b683e4c532
vtegan <vtegan@rochester.rr.com>
2016-03-24 10:32:43 -0400

    Added subclass of Document.

---
+++ test/org/vegan/factory/reflection/LegalDocument.java
@@ -0,0 +1,10 @@
+package org.vegan.factory.reflection;
+
+public class LegalDocument extends Document {
+
+	@Override
+	public void describeDocumentFunction() {
+		System.out.println("Legal Documents are hard to read.  Might make you fall asleep.");
+	}
+
+}
